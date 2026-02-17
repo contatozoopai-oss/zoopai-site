4703d6be6dac4ec9c18576ea471a8f0391672bab
contatozoopai-oss <contato.zoopai@gmail.com>
2026-02-17 10:41:52 -0400

    Update supabase.ts

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -13,18 +13,3 @@ export const supabase = createClient(
     }
   }
 )
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.SUPABASE_URL!
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseServiceRoleKey,
-  {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false
-    }
-  }
-)
